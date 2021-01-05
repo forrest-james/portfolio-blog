5024dd9306ae85377958ec9e9681fe99c91a4184
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-01-05 12:32:18 -0600

    Create DateTimeEncoderService.cs
    Service to display custom date time format

---
+++ source/Application/Common/Services/DateTimeEncoderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Application.Common.Services
+{
+    public static class DateTimeEncoderService
+    {
+        public static string Encode(this DateTime dateTime)
+        {
+            var tempString = new StringBuilder();
+
+            // Convert Year to "Terran Year" (i.e. years since 3000 BCE, recorded history)
+            tempString.Append((dateTime.Year + 3000).ToString());
+
+            // Add Day separator
+            tempString.Append(".");
+
+            // Add three-digit Day of Year
+            tempString.Append(dateTime.DayOfYear.ToString("000"));
+
+            // Add Time separator
+            tempString.Append(".");
+
+            // Add six-digit Time of Day
+            tempString.Append(dateTime.ToString("HHmmss"));
+
+            return tempString.ToString();
+        }
+    }
+}
