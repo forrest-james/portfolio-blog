6d101f61e32dac999249557974dcf94a953eb3dc
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-01-05 12:37:20 -0600

    Create CurrentUserService.cs
    Service to get current user's name

---
+++ source/Application/Common/Services/CurrentUserService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Application.Common.Services
+{
+    public class CurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor) => UserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string UserId { get; }
+    }
+}
