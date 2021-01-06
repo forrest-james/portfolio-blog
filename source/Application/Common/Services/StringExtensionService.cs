c58b88b1d5f0640c188a4670eee4794fda14365c
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-01-06 14:13:46 -0600

    Added string truncation for index and list views

---
+++ source/Application/Common/Services/StringExtensionService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Common.Services
+{
+    public static class StringExtensionService
+    {
+        public static string Truncate(this string text, int length)
+        {
+            if (text == null || text.Length <= length)
+                return text;
+
+            List<char> wordCutOffs = new List<char>()
+            {
+                ' ',
+                ',',
+                '.',
+                '?',
+                '/',
+                ':',
+                ';',
+                '\'',
+                '\"',
+                '-'
+            };
+            string temp = text.Substring(0, length);
+            int lastWhiteSpace = temp.LastIndexOf(' ');
+
+            if (lastWhiteSpace != -1 && (text.Length >= length + 1 && !wordCutOffs.Contains(text.ToCharArray()[length])))
+                temp = temp.Remove(lastWhiteSpace);
+
+            temp += "...";
+
+            return temp;
+        }
+    }
+}
